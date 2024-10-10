58aca22ba40766e9da6d1f0476c552babd7abac0
Karolina Hajkova <karolina.hajkova@tul.cz>
2024-10-10 13:31:46 +0200

    #1 zakladni program s moznosti opakovani

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program {
+
+    static void Main() {
+        
+        //chci aby se program opakoval po stisku klávesy "a"
+        string again = "a";             // operátor = je přiřazení hodnoty
+        while(again == "a") {          // == je porovnávání hodnot
+            Console.Clear();
+            Console.WriteLine("****************************************");
+            Console.WriteLine("************** Výpis řady **************");
+            Console.WriteLine("****************************************");
+            Console.WriteLine("*********** Karolína Hájková ***********");
+            Console.WriteLine("****************************************\n\n");
+            Console.WriteLine();
+
+            // Vstup od uživatele TO-DO
+
+            //Logika pro výpis řady TO-DO
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+
+    }
+
+
+}
+
+
