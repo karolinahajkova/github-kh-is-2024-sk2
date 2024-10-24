8a2cd87decd446c6992c9c52b420442c997dc861
Karolina Hajkova <karolina.hajkova@tul.cz>
2024-10-24 13:30:23 +0200

    #4 generator DONE

--- IS-Projekty/program004a-generator/Program.cs
+++ IS-Projekty/program004a-generator/Program.cs
@@ -14,19 +14,19 @@
             Console.WriteLine();
 
             // Vstup od uživatele TO-DO - správná verze
-            Console.WriteLine("Zadejte počet generovaných čísel (celé číslo): ");
+            Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
             int n;
             while(!int.TryParse(Console.ReadLine(), out n)) {
                 Console.Write("Nezadali jste celé číslo. Zadejte znovu počet čísel (celé číslo): ");
             }     
 
-            Console.WriteLine("Zadejte dolní mez (celé číslo): ");
+            Console.Write("Zadejte dolní mez (celé číslo): ");
             int dm;
             while(!int.TryParse(Console.ReadLine(), out dm)) {
                 Console.Write("Nezadali jste celé číslo. Zadejte znovu dolní mez (celé číslo): ");
             }     
 
-            Console.WriteLine("Zadejte horní mez (celé číslo): ");
+            Console.Write("Zadejte horní mez (celé číslo): ");
             int hm;
             while(!int.TryParse(Console.ReadLine(), out hm)) {
                 Console.Write("Nezadali jste celé číslo. Zadejte znovu horní mez (celé číslo): ");
@@ -37,8 +37,22 @@
             Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez: {2}", n, dm, hm);
             Console.WriteLine("============================================================");
 
+            // deklarace pole
+            int[] myArray = new int[n];
+
+            // příprava pro generování náhodných čísel
+            Random randomNumber = new Random();
+
+            Console.WriteLine("Náhodná čísla: ");
+
+            for(int i=0; i<n; i++) {
+                myArray[i] = randomNumber.Next(dm, hm);
+                Console.Write("{0}; ", myArray[i]);
+            }
+            
+
             // Opakování programu
-            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            Console.WriteLine("\n\nPro opakování programu stiskněte klávesu a");
             again = Console.ReadLine();
         }
 
